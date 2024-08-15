6e06548ffc9e186223d956db567d1baef7bb5046
Saad Husayn <k230705@nu.edu.pk>
2024-08-15 22:32:28 +0500

    initial commit

---
+++ tests.py
@@ -0,0 +1,15 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+import unittest
+
+class CheckTests(unittest.TestCase):
+
+    def test_demo(self):
+        self.assertTrue(True)
+
+    def test_demo2(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
