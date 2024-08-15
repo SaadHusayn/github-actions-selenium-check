387280dad5c99decc4f963036c903dfb8056db03
Saad Husayn <k230705@nu.edu.pk>
2024-08-15 22:58:38 +0500

    made the driver headless

--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import FirefoxOptions
 import unittest
 
-driver = webdriver.Firefox()
+# since server is headless i.e doesnot have screen so we have to consider this
+opts = FirefoxOptions()
+opts.add_argument('--headless')
+
+driver = webdriver.Firefox(options=opts)
 
 class CheckTests(unittest.TestCase):
 
